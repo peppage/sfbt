40a44bbc17832ce9fb474e109530d1e270523c2f
Michael Peppler <peppage@gmail.com>
2016-04-02 10:28:29 -0400

    add javascript minifier

--- sfbt.go
+++ sfbt.go
@@ -10,20 +10,21 @@ import (
 
 	"github.com/pelletier/go-toml"
 	"github.com/tdewolff/minify/css"
+	"github.com/tdewolff/minify/js"
 
 	"github.com/tdewolff/minify"
 )
 
 var cssTargetFile string
+var jsTargetFile string
 var cssWriter io.WriteCloser
+var jsWriter io.WriteCloser
 
 func main() {
 	config, err := toml.LoadFile("conf.toml")
 	if err != nil {
 		log.Fatal("Missing conf.toml")
 	}
-
-	var cssFolder string
 
 	if !config.Has("sfbt.cssFolder") {
 		log.Fatal("config missing css folder")
@@ -33,23 +34,43 @@ func main() {
 		log.Fatal("config missing target css file")
 	}
 
-	cssFolder = config.Get("sfbt.cssFolder").(string)
+	if !config.Has("sfbt.jsFolder") {
+		log.Fatal("config missing js folder")
+	}
+
+	if !config.Has("sfbt.targetJsFile") {
+		log.Fatal("config missing target js file")
+	}
+
+	cssFolder := config.Get("sfbt.cssFolder").(string)
 	cssTargetFile = config.Get("sfbt.targetCssFile").(string)
+	jsFolder := config.Get("sfbt.jsFolder").(string)
+	jsTargetFile = config.Get("sfbt.targetJsFile").(string)
 
 	min := minify.New()
 	min.AddFunc("text/css", css.Minify)
+	min.AddFunc("text/js", js.Minify)
 
 	cssFile, err := os.Create(cssTargetFile)
 	if err != nil {
 		log.Fatal("Failed to create target css file")
 	}
 
+	jsFile, err := os.Create(jsTargetFile)
+	if err != nil {
+		log.Fatal("Failed to create target js file")
+	}
+
 	cssWriter = min.Writer("text/css", cssFile)
+	jsWriter = min.Writer("text/js", jsFile)
 
 	defer cssFile.Close()
 	defer cssWriter.Close()
+	defer jsFile.Close()
+	defer jsWriter.Close()
 
 	filepath.Walk(cssFolder, handleCSS)
+	filepath.Walk(jsFolder, handleJs)
 }
 
 func handleCSS(path string, f os.FileInfo, err error) error {
@@ -60,6 +81,17 @@ func handleCSS(path string, f os.FileInfo, err error) error {
 		_, err = cssWriter.Write(b)
 		check(err)
 
+	}
+	return nil
+}
+
+func handleJs(path string, f os.FileInfo, err error) error {
+	if !f.IsDir() && strings.Contains(f.Name(), "js") {
+		b, err := ioutil.ReadFile(path)
+		check(err)
+
+		_, err = jsWriter.Write(b)
+		check(err)
 	}
 	return nil
 }
